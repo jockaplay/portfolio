2df8d4c332e5e156b7c31455d923efc602b00b62
JockaPlay <74666954+jockaplay@users.noreply.github.com>
2022-02-19 02:30:47 -0300

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,3 +8,12 @@ ButtonMenu.addEventListener("click", () =>{
 ButtonMenuBack.addEventListener("click", () =>{
     MenuSide.classList.toggle("hide");
 })
+function openLink1(){
+    window.open("https://gamejolt.com/games/forgottenarmy/579102", "_blank");
+}
+function openLink2(){
+    window.open("https://gamejolt.com/games/ThiefThrillAlpha/645733", "_blank");
+}
+function openLink3(){
+    window.open("https://gamejolt.com/games/shooterjocka258/650986", "_blank");
+}
